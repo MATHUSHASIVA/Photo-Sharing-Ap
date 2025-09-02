00582efb294f84034fbf1c021582bb92d28e74be
MATHUSHASIVA <mathushasiva2k@gmail.com>
2025-09-02 15:18:24 +0530

    delete unwanted files

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -20,28 +20,3 @@ export default router;
 
 
 
-
-
-// import express from "express";
-// import { 
-//     getUser,
-//     registerUser,
-//     loginUser,
-//     logoutUser,
-//     followUser
-// } from "../controllers/user.controller.js";
-// import { verifyToken } from "../middlewares/verifyToken.js";
-
-
-// const router = express.Router();
-
-
-
-// router.get("/:username", getUser);
-// router.post("/auth/register", registerUser);
-// router.post("/auth/login", loginUser);
-// router.post("/auth/logout", logoutUser);
-// router.post("/follow/:username", verifyToken, followUser);
-
-
-// export default router;
